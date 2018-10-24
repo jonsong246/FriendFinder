7a470b0f076267132736b504834a40250d7fa3a2
Jon Song <jonsong246@gmail.com>
2018-10-23 17:27:49 -0700

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Poo',
+		photo: 'http://disney.wikia.com/wiki/File:Winnie_The_Pooh.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+    },
+    {
+		name: 'Heimlich',
+		photo: 'http://pixar.wikia.com/wiki/File:Heimlich.png',
+		scores: [
+			'5',
+			'2',
+			'1',
+			'1',
+			'4',
+			'1',
+			'5',
+			'3',
+			'3',
+			'2'
+		]
+    },
+    {
+		name: 'Pikachu',
+		photo: 'https://www.kisspng.com/png-pikachu-png-51923/',
+		scores: [
+			'2',
+			'2',
+			'3',
+			'1',
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'1'
+		]
+    },
+    {
+		name: 'Chien Po',
+		photo: 'http://disney.wikia.com/wiki/File:Chien_Po_KHII.png',
+		scores: [
+			'3',
+			'1',
+			'4',
+			'2',
+			'5',
+			'3',
+			'3',
+			'2',
+			'5',
+			'4'
+		]
+	}
+]
+module.exports = friendsArray;
